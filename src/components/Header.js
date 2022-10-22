8b3fe55530647281af1b3e5b626c18a62e9b29f3
Tarık <feanorwastaken@gmail.com>
2022-10-22 21:15:26 +0300

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className="header">
+        <img src="./images/trollface.png" className="header--image" alt="img" />
+        <h2 className="header--title">Meme Generator</h2>
+        </header>
+    )
+}
